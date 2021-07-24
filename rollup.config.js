56141e0c656103f9a4463b339491fd278ce4091d
Alex Bosch <bosch@parc.app>
2021-07-25 00:41:47 +0200

    feat: ignore node_modules & example folder in package.

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ const pkg = require('./package.json');
 export default {
     input: 'src/Marquee.svelte',
     output: [
-        { file: pkg.module, 'format': 'en' },
+        { file: pkg.module, 'format': 'es' },
         { file: pkg.main, 'format': 'umd', name: 'Name' }
     ],
     plugins: [
