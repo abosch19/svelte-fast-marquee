554abecbe1eac4d4fca6e0df275907b36a0eed2e
Alex Bosch <abosch19@gmail.com>
2023-09-11 11:10:30 +0800

    chore: replace example folder by sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Marquee from "../lib/Marquee.svelte";
+</script>
+
+<Marquee gradient --gradientColor="blue">
+	<h1>Selvet FAST Marquee</h1>
+</Marquee>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte({ emitCss: false })],
+	plugins: [sveltekit()],
 })
