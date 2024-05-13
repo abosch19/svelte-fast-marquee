5cef2384d2935bb093413fcc4b758dd4d3a33408
Nicolas Montavon <143720818+vyconm@users.noreply.github.com>
2024-05-13 16:19:31 +0200

    Update Marquee.svelte to add closing tag
    I think this is literally it?

--- src/lib/Marquee.svelte
+++ src/lib/Marquee.svelte
@@ -73,7 +73,7 @@
 	style:--pause-on-click={pauseOnClick ? "paused" : "running"}
 >
 	{#if gradient}
-		<div class="gradient" data-testid="marquee-gradient" />
+		<div class="gradient" data-testid="marquee-gradient"></div>
 	{/if}
 	<div class="marquee" bind:clientWidth={marqueeWidth} data-testid="marquee-slot">
 		<slot />
